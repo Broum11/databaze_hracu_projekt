9433b8e6b8edadb8fe3cd84e433c4b3e2c04bfd0
Broum11 <165827478+Broum11@users.noreply.github.com>
2025-02-16 08:43:01 +0100

    Ošetření chyby

--- Chance_liga/urls.py
+++ Chance_liga/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.domu, name='domu'),
     path('tabulka/', views.tabulka, name='tabulka'),
     path('seznam_tymu/', views.seznam_tymu, name='seznam_tymu'),
-    path('team/<int:team_id>', views.soupiska_hracu, name='soupiska_hracu'),
+    path('team/<int:team_id>/', views.soupiska_hracu, name='soupiska_hracu'),
 ]
--- Chance_liga/views.py
+++ Chance_liga/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . models import Team, Chart, Player
+from django.http import HttpResponseNotFound
 
 def domu(request):
     return render(request, 'domu.html') # render je funkce, která slouží k vykreslení HTML šablony na základě zadaných dat
@@ -13,6 +14,9 @@ def tabulka(request):
     return render(request, 'tabulka.html', {'teams': teams})
 
 def soupiska_hracu(request,team_id):
-    players = Player.objects.all() # Získání všech hráčů pro tento tým
-    team = Team.objects.get(id=team_id) # vrací tým na který kliknu, podle id
-    return render(request, 'soupiska_hracu.html', {'team': team, 'players': players})
+    try:
+        players = Player.objects.all() # Získání všech hráčů pro tento tým
+        team = Team.objects.get(id=team_id) # vrací tým na který kliknu, podle id
+        return render(request, 'soupiska_hracu.html', {'team': team, 'players': players})
+    except:
+        return HttpResponseNotFound('<h1>Tým s tímto ID neexistuje</h1>')
